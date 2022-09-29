ee5c8c86519b6a5d47a4e84b00ac3ead4a12103a
Jolonte <vinicius.nevesco@gmail.com>
2022-09-29 20:01:42 -0300

    :sparkles: feat: Style 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,25 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>404</h1>
-  <h2>Page not found</h2>
+  <main class="flex flex-col items-center justify-center h-full p-6 gap-2">
+    <h1>Oops!</h1>
+    <h1>Alguma deu errada :(</h1>
+  </main>
+  <footer class="fixed w-full bottom-0">
+    <!-- navigation bar -->
+    <nav
+      class="flex flex-col bg-slate-100 shadow-sm md:px-48 lg:px-80 xl:px-96"
+    >
+      <div class="flex w-full">
+        <div class="flex flex-col items-center w-2/4 pt-4 pb-3 gap-2">
+          <i class="material-symbols-rounded">home</i>
+          <p class="font-medium text-sm">Home</p>
+        </div>
+        <div class="flex flex-col items-center w-2/4 pt-4 pb-3 gap-2">
+          <i class="material-symbols-rounded">add</i>
+          <p class="font-medium text-sm">Cadastrar</p>
+        </div>
+      </div>
+      <div class="w-full h-4"></div>
+    </nav>
+  </footer>
 </template>
