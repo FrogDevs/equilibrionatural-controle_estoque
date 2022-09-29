8357e48d30028c647d7d7d36d8f9c82a2d05c11a
Jolonte <vinicius.nevesco@gmail.com>
2022-09-29 18:15:52 -0300

    :sparkles: feat: Stores fixes

--- src/views/Stores.vue
+++ src/views/Stores.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <!-- escolher lojas -->
 <template>
   <main class="flex flex-col items-center p-6">
@@ -6,7 +7,7 @@
       class="flex flex-col w-full mt-6 rounded-xl border border-solid border-slate-700"
     >
       <div class="w-full h-44 rounded-t-xl bg-blue-400"></div>
-      <div class="flex flex-col w-full h-44 p-5 rounded-b-xl gap-5">
+      <div class="flex flex-col w-full p-5 rounded-b-xl gap-5">
         <h1 class="text-2xl">Rua imberé, 373</h1>
         <p class="text-sm">Unidade 1</p>
       </div>
@@ -15,7 +16,7 @@
       class="flex flex-col w-full mt-6 rounded-xl border border-solid border-slate-700"
     >
       <div class="w-full h-44 rounded-t-xl bg-blue-400"></div>
-      <div class="flex flex-col w-full h-44 p-5 rounded-b-xl gap-5">
+      <div class="flex flex-col w-full p-5 rounded-b-xl gap-5">
         <h1 class="text-2xl">Av. Taquandava, 1203</h1>
         <p class="text-sm">Unidade 2</p>
       </div>
