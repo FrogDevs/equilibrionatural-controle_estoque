31120c6807b2f39dccbb266831efde5051d4fdb1
Jolonte <vinicius.nevesco@gmail.com>
2022-09-29 18:25:06 -0300

    :sparkles: feat: Search fixes II

--- src/views/products/Search.vue
+++ src/views/products/Search.vue
@@ -17,19 +17,30 @@
       <i class="material-symbols-rounded">arrow_back</i>
     </nav>
   </header>
-  <main class="flex flex-col w-full overflow-y-scroll mt-20">
-    <div class="flex flex-col">
-      <hr class="h-2 my-2 bg-slate-100 border-none" />
-    </div>
-    <section class="flex-col px-6">
-      <h1 class="font-normal text-2xl">A</h1>
-      <div
-        class="flex flex-col w-full mt-2 rounded-xl border border-solid border-slate-700"
-      >
-        <div class="w-full h-44 rounded-t-xl bg-blue-400"></div>
-        <div class="flex flex-col w-full p-5 rounded-b-xl gap-5">
-          <h1 class="text-2xl">Nome</h1>
-          <p class="text-sm">20</p>
+  <main class="flex flex-col w-full overflow-y-scroll mt-20 my-6">
+    <section class="flex-col">
+      <div class="flex flex-col">
+        <hr class="h-2 my-2 bg-slate-100 border-none" />
+      </div>
+      <h1 class="font-normal text-2xl ml-6">A</h1>
+      <div class="flex flex-col pt-3 px-6 gap-6">
+        <div
+          class="flex flex-col w-full mt-2 rounded-xl border border-solid border-slate-700"
+        >
+          <div class="w-full h-44 rounded-t-xl bg-blue-400"></div>
+          <div class="flex flex-col w-full p-5 rounded-b-xl gap-5">
+            <h1 class="text-2xl">Nome</h1>
+            <p class="text-sm">20</p>
+          </div>
+        </div>
+        <div
+          class="flex flex-col w-full mt-2 rounded-xl border border-solid border-slate-700"
+        >
+          <div class="w-full h-44 rounded-t-xl bg-blue-400"></div>
+          <div class="flex flex-col w-full p-5 rounded-b-xl gap-5">
+            <h1 class="text-2xl">Nome</h1>
+            <p class="text-sm">20</p>
+          </div>
         </div>
       </div>
     </section>
