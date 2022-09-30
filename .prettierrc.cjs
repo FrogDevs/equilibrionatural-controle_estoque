3d4e960861d767d72b3d059b2b42a2349d6b5b5a
Jolonte <vinicius.nevesco@gmail.com>
2022-09-30 20:17:44 -0300

    :sparkles: feat: Tailwind plugin

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,5 +2,7 @@ module.exports = {
     semi: false,
     singleQuote: true,
     tabWidth: 2,
-    trailingComma: 'none'
+    trailingComma: 'none',
+    plugins: [require('prettier-plugin-tailwindcss')],
+    tailwindConfig: 'tailwind.config.cjs',
 }
