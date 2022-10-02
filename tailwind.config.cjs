bd16013ccb189ad5473a3c7dc65795939994206b
Jolonte <vinicius.nevesco@gmail.com>
2022-10-02 14:40:13 -0300

    :recycle: Refactor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     backgroundImage: {
-      store1: "url('public/store1.avif')",
-      store2: "url('public/store2.avif')"
+      store1: "url('store1.avif')",
+      store2: "url('store2.avif')"
     }
   },
   plugins: []
