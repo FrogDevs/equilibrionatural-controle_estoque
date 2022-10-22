20d34a74c8ee1b1722995ea94c7af6ba940411b0
Jolonte <vinicius.nevesco@gmail.com>
2022-10-22 08:26:43 -0300

    :lipstick: feat: UserLogin UI

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import { ref, computed } from 'vue'
 import TheDialogue from './components/TheDialogue.vue'
 
-const online = ref(window.navigator.onLine)
-
 const wifiState = computed(() => {
+  const online = ref(window.navigator.onLine)
+
   if (online.value) {
     return false
   } else {
@@ -17,13 +17,13 @@ function reloadPage() {
 }
 </script>
 <template>
-  <!-- <the-dialogue
+  <the-dialogue
     v-if="wifiState"
     :dialogue-icon="true"
     title="Sem conexão Wifi"
     message="Parece que você está sem conexão com uma rede Wifi. Dados importantes podem não aparecer e funções do sistema não funcionarem da maneira esperada. Certifique-se de estabelecer uma conexão válida."
     button-two-title="Reconectar"
     @primary-action="reloadPage"
-  /> -->
+  />
   <router-view />
 </template>
--- src/views/UserLogin.vue
+++ src/views/UserLogin.vue
@@ -3,25 +3,65 @@ import TextField from '../components/TextField.vue'
 import TheButton from '../components/TheButton.vue'
 </script>
 <template>
-  <header class="flex flex-col gap-1 px-6 pt-14">
-    <div class="flex items-center gap-1">
-      <h1 class="text-2xl font-semibold">Login</h1>
-      <i class="material-symbols-rounded"> login </i>
-    </div>
-    <p class="text-sm">Bem-vindo de volta!</p>
-  </header>
-  <main
-    class="flex w-full shrink grow flex-col items-center justify-center py-14 px-6"
-  >
-    <div class="flex flex-col gap-4">
-      <h1 class="text-center text-5xl font-medium">🪴 Equilíbrio</h1>
-      <h1 class="text-center text-5xl font-bold">Natural 🍃</h1>
-    </div>
-    <form class="mt-12 flex w-full flex-col items-center gap-6" @submit.prevent>
-      <TextField title="Acesso chave" input-type="text" />
-      <TheButton title="Entrar" />
-    </form>
-    <p class="mt-12 hover:cursor-pointer">Entrar como visitante</p>
-  </main>
+  <!-- img -->
+  <div
+    class="absolute top-0 hidden h-screen w-2/4 bg-store2 bg-cover lg:block"
+  ></div>
+
+  <div class="absolute top-0 right-0 flex h-screen w-full flex-col lg:w-2/4">
+    <header class="z-20 flex w-full flex-row p-6 lg:p-0">
+      <div class="flex w-full flex-col gap-1 lg:w-2/4 lg:p-6">
+        <div class="flex items-center gap-1">
+          <h1 class="text-2xl font-semibold text-amber-800">Login</h1>
+          <i class="material-symbols-rounded text-amber-800"> login </i>
+        </div>
+        <p class="text-sm text-amber-800">Bem-vindo de volta!</p>
+      </div>
+    </header>
+    <main
+      class="relative z-20 flex h-full w-full shrink grow flex-row items-center justify-center py-14 px-6 lg:px-0"
+    >
+      <div class="flex w-full flex-col items-center">
+        <div class="flex flex-col gap-4">
+          <h1 class="text-center text-5xl font-medium text-amber-700">
+            🪴 Equilíbrio
+          </h1>
+          <h1 class="text-center text-5xl font-bold text-amber-700">
+            Natural 🍃
+          </h1>
+        </div>
+        <form
+          class="mt-12 flex w-full flex-col items-center gap-6 lg:w-2/4"
+          @submit.prevent
+        >
+          <TextField title="Chave de acesso" input-type="text" />
+          <TheButton title="Entrar" />
+        </form>
+        <p
+          class="mt-12 text-amber-800 transition-colors duration-200 ease-in-out hover:cursor-pointer hover:text-opacity-80 active:text-opacity-60"
+        >
+          Entrar como visitante
+        </p>
+      </div>
+    </main>
+  </div>
+
   <!-- decorations -->
+  <div class="absolute top-0 z-10 w-full sm:hidden">
+    <div class="flex h-16 bg-green-300 bg-opacity-20"></div>
+    <div class="bg-clip-1 flex h-80 bg-green-300 bg-opacity-20"></div>
+  </div>
+  <div
+    class="bg-clip-2 absolute top-[-1.5rem] flex h-96 w-full bg-green-300 bg-opacity-20 sm:hidden"
+  ></div>
 </template>
+
+<style scoped>
+.bg-clip-1 {
+  clip-path: ellipse(50% 50% at 50% 0%);
+}
+
+.bg-clip-2 {
+  clip-path: ellipse(50% 50% at 95% 0%);
+}
+</style>
