7b748a8aaa7453a343f85d56d1b51ce0d97b168f
Jolonte <vinicius.nevesco@gmail.com>
2022-10-23 20:57:37 -0300

    :recycle: refactor: Removed imgs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,9 +9,7 @@ module.exports = {
       category4: "url('/category4.avif')",
       category5: "url('/category5.avif')",
       store1: "url('/store1.avif')",
-      store2: "url('/store2.avif')",
-      store3: "url('/store3.avif')",
-      store4: "url('/store4.avif')"
+      store2: "url('/store2.avif')"
     }
   },
   plugins: []
