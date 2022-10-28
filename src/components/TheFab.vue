994a00975a36e81a88a0abd23576d6c8dc1d4240
Jolonte <vinicius.nevesco@gmail.com>
2022-10-27 21:36:33 -0300

    :lipstick: fix: Removed animation

--- src/components/TheFab.vue
+++ src/components/TheFab.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 <template>
   <router-link :to="`/${props.market}/${props.category}/registration`">
     <button
-      class="flex items-center rounded-2xl bg-green-300 p-4 shadow-md drop-shadow-md transition-all duration-200 ease-in-out hover:bg-opacity-80 hover:drop-shadow-xl active:rounded-3xl active:bg-opacity-70"
+      class="flex items-center rounded-2xl bg-green-300 p-4 shadow-md drop-shadow-md transition-all duration-200 ease-in-out hover:bg-opacity-80 hover:drop-shadow-xl active:bg-opacity-70"
     >
       <i class="material-symbols-rounded text-amber-800">Add</i>
     </button>
