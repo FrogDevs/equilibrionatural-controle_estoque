7f0a773625d0d50fc148eeeff016601847115d9b
Jolonte <vinicius.nevesco@gmail.com>
2022-11-01 08:22:05 -0300

    :bug: fix: Toggle modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { defineAsyncComponent } from 'vue'
+import { ref, defineAsyncComponent } from 'vue'
 import NavigationBar from '../components/NavigationBar.vue'
 import ToolBar from '../components/ToolBar.vue'
 import TheDivider from '../components/TheDivider.vue'
-// import SearchModal from '../components/SearchModal.vue'
+import SearchModal from '../components/SearchModal.vue'
 
 const asyncList = defineAsyncComponent(() =>
   import('../components/TheList.vue')
@@ -20,6 +20,8 @@ const props = defineProps({
     default: ''
   }
 })
+
+const modal = ref(false)
 </script>
 <template>
   <header class="fixed top-0 z-10 w-full">
@@ -28,11 +30,11 @@ const props = defineProps({
   <main class="flex flex-col items-center pb-24">
     <div
       class="mx-2 mt-20 flex w-fit cursor-pointer gap-4 rounded-2xl bg-green-100 py-3 px-4 transition-colors duration-200 ease-in-out hover:bg-opacity-80 active:bg-opacity-70"
+      @click="modal = true"
     >
       <i class="material-symbols-rounded text-amber-700">search</i>
       <p class="w-[15.75rem] text-amber-700">Pesquisar um produto</p>
     </div>
-
     <TheDivider subtitle="Categorias" />
     <section class="flex w-full flex-col gap-2 px-2">
       <asyncList
@@ -75,5 +77,5 @@ const props = defineProps({
   <footer class="fixed bottom-0 w-full">
     <NavigationBar :home="true" :user="props.user" :market="props.market" />
   </footer>
-  <!-- <SearchModal /> -->
+  <SearchModal v-if="modal" @close-modal="modal = false" />
 </template>
