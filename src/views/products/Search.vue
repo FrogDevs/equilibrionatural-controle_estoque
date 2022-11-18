0786e53831b94c36f2fb7ae94864f511b503b925
Jolonte <vinicius.nevesco@gmail.com>
2022-11-18 16:53:50 -0300

    :sparkles: feat: Search details

--- src/views/products/Search.vue
+++ src/views/products/Search.vue
@@ -5,6 +5,7 @@ import { useProductStore } from '../../stores/ProductStore'
 import { useUserStore } from '../../stores/UserStore'
 import ToolBar from '../../components/ToolBar.vue'
 import TheFab from '../../components/TheFab.vue'
+import TheDivider from '../../components/TheDivider.vue'
 
 const AsyncCard = defineAsyncComponent(() =>
   import('../../components/TheCard.vue')
@@ -41,6 +42,20 @@ const itemSubtitle = (item) => {
     return null
   }
 }
+
+const itemCategory = computed(() => {
+  if (props.category === 'alimenticios') {
+    return 'Alimentícios'
+  } else if (props.category === 'beleza') {
+    return 'Beleza'
+  } else if (props.category === 'oleos') {
+    return 'Óleos essenciais'
+  } else if (props.category === 'suplementos') {
+    return 'Suplementos'
+  } else {
+    return 'Vida & Saúde'
+  }
+})
 </script>
 
 <template>
@@ -48,6 +63,13 @@ const itemSubtitle = (item) => {
     <ToolBar :user="props.user" :market="props.market" />
   </header>
   <main class="mt-20 flex h-full w-full flex-col overflow-y-scroll pb-24">
+    <p v-if="!piniaData.length" class="mx-6 text-tertiary">
+      Sem produtos estocados em {{ itemCategory }}.
+    </p>
+    <p v-else class="mx-6 text-tertiary">
+      {{ piniaData.length }} produto(s) estocados em {{ itemCategory }}
+    </p>
+    <TheDivider />
     <div
       class="auto-grid flex flex-col items-center justify-center gap-4 px-4 md:grid"
     >
@@ -66,9 +88,6 @@ const itemSubtitle = (item) => {
           <p>Carregando...</p>
         </template>
       </Suspense>
-      <p v-if="!piniaData.length" class="text-tertiary">
-        Sem itens registrados.
-      </p>
     </div>
   </main>
   <footer class="fixed bottom-0 flex w-full justify-end p-4">
