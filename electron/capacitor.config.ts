af0b898efa8a31f741bd26e9f0b5cbc2e232af92
Jolonte <vinicius.nevesco@gmail.com>
2022-11-28 18:44:20 -0300

    :sparkles: Desktop app

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.equilibrionatural.app',
+  appName: 'Equilibrio Natural',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
